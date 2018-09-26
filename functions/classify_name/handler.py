eaf01bd745d448f00c866d059bd69e9b229cce4c
ngautier <nclgautier@gmail.com>
2018-09-26 10:07:13 +0200

    Fixed function docker image.

--- functions/classify_name/handler.py
+++ functions/classify_name/handler.py
@@ -2,40 +2,41 @@ import datetime
 import importlib
 import json
 import os
+import sys
 import torch
-import typing 
 
-from common import utils
 from pathlib import PurePath
 from torch.autograd import Variable
-
-importlib.reload(utils)
+from typing import List, Any
 
 bundle_root = os.environ.get('LABS_BUNDLE_ROOT', '/labs')
+sys.path.append(str(PurePath(bundle_root, 'common')))
+import utils
+importlib.reload(utils)
 
-# define RNN class (necessary to load the trained model)
+# load base classe
 RNN = utils.RNN
+# init model
+rnn = RNN(utils.n_letters, utils.n_hidden, utils.n_categories)
+# fill in weights
+rnn.load_state_dict(torch.load(str(PurePath(bundle_root, 'common/char-rnn-classification.pt'))))
 
-# load rnn 
-rnn = torch.load(str(PurePath(bundle_root, 'data/processed/char-rnn-classification.pt')))
-
-
-def predict(line: str, n_predictions: int=3) ->str:
+def predict(line: str, n_predictions: int=3) -> List[Any]:
     output = utils.evaluate(Variable(utils.lineToTensor(line)), rnn)
 
     # Get top N categories
     topv, topi = output.data.topk(n_predictions, 1, True)
-    predictions = []
+    predictions: List[Any] = []
 
     for i in range(n_predictions):
         value = topv[0][i]
         category_index = topi[0][i]
-        predictions.append([str(value).split('tensor')[1], utils.all_categories[category_index]])
+        predictions += [(str(value).split('tensor')[1], utils.all_categories[category_index])]
 
     return predictions
         
 
-def handle(input_string: str) -> None:
+def handle(input_string: str) -> str:
     # handle empty input
     if not input_string:
         return 'No input provided'

fe7ccdb6fcb9ddf881fdd0ff45ab6f835001cbba
ngautier <nclgautier@gmail.com>
2018-11-09 09:00:13 +0100

    Changed edit image to prod.

--- functions/classify_name/index.py
+++ functions/classify_name/index.py
@@ -10,8 +10,7 @@ bundle_root = os.environ.get('LABS_BUNDLE_ROOT', '/labs/project')
 
 
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+
 
 
 print('Server starting')
@@ -19,21 +18,14 @@ print('Server starting')
 
 @app.route("/", methods=["POST", "GET"])
 def main_route():
-    logger.debug("running main_route")
-    try:
-        print('Calling handler')
-        data = request.get_data()
-        print(data, type(data), request.data, request.get_json(), request.form, str(data))
-        ret = handler.handle(str(data))
-        return ret
-    except Exception as e:
-        # get all error arguments as strings
-        whole_error = ''
-        for arg in e.args:
-            whole_error += ' ' + arg
-        return whole_error
+    print('Calling handler')
+    data = request.get_data()
+    print(data, type(data), request.data, request.get_json(), request.form, str(data))
+    ret = handler.handle(data)
+    print('Output from function is {}'.format(ret))
+    return ret
+
 
 
 if __name__ == '__main__':
-    logger.debug("starting function server at 0.0.0.0:5000")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
--- functions/convert.py
+++ functions/convert.py
@@ -9,9 +9,17 @@ import torch
 from torchvision.models import squeezenet1_1
 
 
+def add_this(a: int, b: int) -> int:
+    return a + b
+
+
+def use_add_this() -> int:
+    return add_this(1, 'hello')
+
+
 class PytorchToKeras(object):
-    def __init__(self,pModel,kModel):
-        super(PytorchToKeras,self)
+    def __init__(self, pModel, kModel):
+        super(PytorchToKeras, self)
         self.__source_layers = []
         self.__target_layers = []
         self.pModel = pModel
@@ -21,11 +29,11 @@ class PytorchToKeras(object):
 
     def __retrieve_k_layers(self):
 
-        for i,layer in enumerate(self.kModel.layers):
+        for i, layer in enumerate(self.kModel.layers):
             if len(layer.weights) > 0:
                 self.__target_layers.append(i)
 
-    def __retrieve_p_layers(self,input_size):
+    def __retrieve_p_layers(self, input_size):
 
         input = torch.randn(input_size)
 
@@ -36,24 +44,23 @@ class PytorchToKeras(object):
         def add_hooks(module):
 
             def hook(module, input, output):
-                if hasattr(module,"weight"):
+                if hasattr(module, "weight"):
                     self.__source_layers.append(module)
 
-            if not isinstance(module, nn.ModuleList) and not isinstance(module,nn.Sequential) and module != self.pModel:
+            if not isinstance(module, nn.ModuleList) and not isinstance(module, nn.Sequential) and module != self.pModel:
                 hooks.append(module.register_forward_hook(hook))
 
         self.pModel.apply(add_hooks)
-
 
         self.pModel(input)
         for hook in hooks:
             hook.remove()
 
-    def convert(self,input_size):
+    def convert(self, input_size):
         self.__retrieve_k_layers()
         self.__retrieve_p_layers(input_size)
 
-        for i,(source_layer,target_layer) in enumerate(zip(self.__source_layers,self.__target_layers)):
+        for i, (source_layer, target_layer) in enumerate(zip(self.__source_layers, self.__target_layers)):
 
             weight_size = len(source_layer.weight.data.size())
 
@@ -64,10 +71,8 @@ class PytorchToKeras(object):
 
             self.kModel.layers[target_layer].set_weights([source_layer.weight.data.numpy().transpose(transpose_dims), source_layer.bias.data.numpy()])
 
-    def save_model(self,output_file):
+    def save_model(self, output_file):
         self.kModel.save(output_file)
-    def save_weights(self,output_file):
+
+    def save_weights(self, output_file):
         self.kModel.save_weights(output_file)
-
-
-

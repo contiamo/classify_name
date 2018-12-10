52b4b8116de87c34094297d98cdae497dc331653
ngautier <nclgautier@gmail.com>
2018-12-10 18:58:42 +0100

    Fixed dockerfile.

--- functions/index_by_nationality/index.py
+++ functions/index_by_nationality/index.py
@@ -1,31 +1,13 @@
-import logging
-import os
-
-import handler
 from flask import Flask, request
-
-print('Index starting')
-
-bundle_root = os.environ.get('LABS_BUNDLE_ROOT', '/labs/project')
-
+import handler
 
 app = Flask(__name__)
 
-
-
-print('Server starting')
-
-
 @app.route("/", methods=["POST", "GET"])
 def main_route():
-    print('Calling handler')
-    data = request.get_data()
-    print(data, type(data), request.data, request.get_json(), request.form, str(data))
+    data = request.get_data().decode('ascii')
     ret = handler.handle(data)
-    print('Output from function is {}'.format(ret))
     return ret
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
